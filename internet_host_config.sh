f0c45e3bcc1bde6481125cff0d5d9f9d0a33ceb7
GuinRodrigues <guir6462@gmail.com>
2026-03-20 23:35:06 +0000

    Alteracao internet_host e dmz_host

--- internet_host_config.sh
+++ internet_host_config.sh
@@ -1,6 +1,6 @@
 # Set up interface
-ifconfig enp0s8 193.136.0.1 netmask 255.255.0.0 up
-ip route add default via 193.136.0.254
+ifconfig enp0s8 193.136.212.1 netmask 255.0.0.0 up
+ip route add default via 87.248.214.97
 
 # Clear Iptables
 iptables -F
